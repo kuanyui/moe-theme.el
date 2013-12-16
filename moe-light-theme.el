32430b15d87c7bd48ba25a86cbab1ffcbc6136b0
kuanyui <azazabc123@gmail.com>
2013-12-16 20:17:06 +0800

    Font lock of comments and so on.

--- moe-light-theme.el
+++ moe-light-theme.el
@@ -69,8 +69,8 @@ Moe, moe, kyun!")
 
    ;; Font lock faces
    `(font-lock-builtin-face ((,class (:foreground ,purple-2))))
-   `(font-lock-comment-delimiter-face ((,class (:foreground ,red-2 :slant italic))))
-   `(font-lock-comment-face ((,class (:foreground ,red-2 :slant italic))))
+   `(font-lock-comment-delimiter-face ((,class (:foreground ,white-4 :slant italic))))
+   `(font-lock-comment-face ((,class (:foreground ,white-4 :slant italic))))
    `(font-lock-constant-face ((,class (:foreground ,blue-2))))
    `(font-lock-doc-face ((,class (:foreground ,red-2))))
    `(font-lock-doc-string-face ((,class (:foreground ,yellow-3))))
