7f6024590077d0349ff0ea7a359c89acdd156b5f
Jonne Mickelin <jonne@ljhms.se>
2025-01-30 12:47:22 +0100

    Use defvar to declare variables and prefix top-level values with module name
    Call `defvar` for each variable used in this module, to fix
    byte-compile warnings.
    
    Also prefix all top-level variables and functions with the module
    name, to avoid polluting the global namespace.
    
    Fixes #72

--- moe-theme-switcher.el
+++ moe-theme-switcher.el
@@ -41,30 +41,37 @@ Take Keelung, Taiwan(25N,121E) for example, you can set like this:
 	(setq calendar-longitude +121)"
 )
 
-(defvar moe-theme-which-enabled nil
+(defvar moe-theme-switcher--which-enabled nil
   "Variable indicate which theme (moe-dark or light) is being used.")
 
-(defun moe-load-theme (switch-to)
+(defvar moe-theme-switcher--24h/sunrise)
+
+(defvar moe-theme-switcher--24h/sunset)
+
+(defvar moe-theme-switcher--timer nil
+  "Timer for checking whether to switch themes between `moe-light' and `moe-dark'.")
+
+(defun moe-theme-switcher--load-theme (switch-to)
   "Avoid unnecessary load-theme and screen flashing in GUI version Emacs"
   (cond ((equal switch-to 'light)
-         (if (not (equal moe-theme-which-enabled 'light))
+         (if (not (equal moe-theme-switcher--which-enabled 'light))
            (progn (moe-light)
-                  (setq moe-theme-which-enabled 'light)))) ;[FIXME] Maybe unnecessary
+                  (setq moe-theme-switcher--which-enabled 'light)))) ;[FIXME] Maybe unnecessary
         ((equal switch-to 'dark)
-         (if (not (equal moe-theme-which-enabled 'dark))
+         (if (not (equal moe-theme-switcher--which-enabled 'dark))
            (progn (moe-dark)
-                  (setq moe-theme-which-enabled 'dark)))))) ;[FIXME] Maybe unnecessary
+                  (setq moe-theme-switcher--which-enabled 'dark)))))) ;[FIXME] Maybe unnecessary
 
-(defun switch-at-fixed-time ()
+(defun moe-theme-switcher--switch-at-fixed-time ()
   (let ((now (string-to-number (format-time-string "%H"))))
     (if (and (>= now 06) (<= now 18))
-        (moe-load-theme 'light)
-      (moe-load-theme 'dark))
+        (moe-theme-switcher--load-theme 'light)
+      (moe-theme-switcher--load-theme 'dark))
     nil))
 
 ;; (Thanks for letoh!)
 ;; Fix strange bahavior of sunrise-sunset when buffer's width is too narrow.
-(defun get-sunrise-sunset-string ()
+(defun moe-theme-switcher--get-sunrise-sunset-string ()
   "get the real result from `sunrise-sunset'"
   (save-window-excursion
     (let ((regex "[0-9]+:[0-9]+[ap]m")
@@ -83,17 +90,17 @@ Take Keelung, Taiwan(25N,121E) for example, you can set like this:
 
 ;; Convert am/pm to 24hr and save to 24h/sunrise & 24h/set
 ;; Excute every 24 hr
-(defun convert-time-format-of-sunrise-and-sunset ()
+(defun moe-theme-switcher--compute-sunrise-sunset ()
   (let (rise_set a b c d e f)
-    (setq rise_set (get-sunrise-sunset-string))
+    (setq rise_set (moe-theme-switcher--get-sunrise-sunset-string))
     (if (string-match "0:00 hours daylight" rise_set) ;If polar-night
         (progn
-          (setq 24h/sunrise 'polar-night
-                24h/sunset 'polar-night))
+          (setq moe-theme-switcher--24h/sunrise 'polar-night
+                moe-theme-switcher--24h/sunset 'polar-night))
       (if (string-match "24:00 hours daylight" rise_set) ;If midnight-sun
           (progn
-            (setq 24h/sunrise 'midnight-sun
-                  24h/sunset 'midnight-sun))
+            (setq moe-theme-switcher--24h/sunrise 'midnight-sun
+                  moe-theme-switcher--24h/sunset 'midnight-sun))
         (progn                          ;Convert 12hr to 24hr
           (string-match "\\([0-9][0-9]?\\):\\([0-9][0-9]\\)\\([ap]m\\).+\\([0-9][0-9]?\\):\\([0-9][0-9]\\)\\([ap]m\\)" rise_set)
           (setq a (string-to-number (match-string 1 rise_set))
@@ -103,41 +110,41 @@ Take Keelung, Taiwan(25N,121E) for example, you can set like this:
                e (string-to-number (match-string 5 rise_set))
                f (match-string 6 rise_set))
          (if (equal c "pm")
-             (setq 24h/sunrise (list (+ 12 a) b))
-           (setq 24h/sunrise (list a b)))
+             (setq moe-theme-switcher--24h/sunrise (list (+ 12 a) b))
+           (setq moe-theme-switcher--24h/sunrise (list a b)))
          (if (equal f "pm")
-             (setq 24h/sunset (list (+ 12 d) e))
-           (setq 24h/sunset (list d e))))))))
+             (setq moe-theme-switcher--24h/sunset (list (+ 12 d) e))
+           (setq moe-theme-switcher--24h/sunset (list d e))))))))
 
 ;; Excute every minute.
-(defun switch-by-locale ()
-  (if (equal 24h/sunrise 'polar-night)  ;If polar-night...moe-dark!
-      (moe-load-theme 'dark)
-    (if (equal 24h/sunrise 'midnight-sun) ;If midnight-sun...moe-light!
-        (moe-load-theme 'light)
+(defun moe-theme-switcher--switch-by-locale ()
+  (if (equal moe-theme-switcher--24h/sunrise 'polar-night)  ;If polar-night...moe-dark!
+      (moe-theme-switcher--load-theme 'dark)
+    (if (equal moe-theme-switcher--24h/sunrise 'midnight-sun) ;If midnight-sun...moe-light!
+        (moe-theme-switcher--load-theme 'light)
       (progn
        (let ((now (list (string-to-number (format-time-string "%H"))
                         (string-to-number (format-time-string "%M")))))
          (if (and (or                        ;magic-logic [tm]
-                   (> (car now) (car 24h/sunrise))
+                   (> (car now) (car moe-theme-switcher--24h/sunrise))
                    (and
-                    (= (car now) (car 24h/sunrise))
-                    (>= (cadr now) (cadr 24h/sunrise))))
+                    (= (car now) (car moe-theme-switcher--24h/sunrise))
+                    (>= (cadr now) (cadr moe-theme-switcher--24h/sunrise))))
                   (or
-                   (< (car now) (car 24h/sunset))
+                   (< (car now) (car moe-theme-switcher--24h/sunset))
                    (and
-                    (= (car now) (car 24h/sunset))
-                    (< (cadr now) (cadr 24h/sunset)))))
-             (moe-load-theme 'light)
-           (moe-load-theme 'dark)
+                    (= (car now) (car moe-theme-switcher--24h/sunset))
+                    (< (cadr now) (cadr moe-theme-switcher--24h/sunset)))))
+             (moe-theme-switcher--load-theme 'light)
+           (moe-theme-switcher--load-theme 'dark)
            ))))))
 
-(defun moe-theme-auto-switch ()
+(defun moe-theme-switcher--auto-switch ()
   "Automatically switch between dark and light moe-theme."
   (interactive)
-  (if (boundp '24h/sunrise)
-      (switch-by-locale)
-    (switch-at-fixed-time))
+  (if (boundp 'moe-theme-switcher--24h/sunrise)
+      (moe-theme-switcher--switch-by-locale)
+    (moe-theme-switcher--switch-at-fixed-time))
   )
 
 (if (and
@@ -145,15 +152,15 @@ Take Keelung, Taiwan(25N,121E) for example, you can set like this:
      (boundp 'calendar-latitude)
      (eql moe-theme-switch-by-sunrise-and-sunset t))
     (progn
-      (run-with-timer 0 (* 60 60 24) 'convert-time-format-of-sunrise-and-sunset))
+      (run-with-timer 0 (* 60 60 24) 'moe-theme-switcher--compute-sunrise-sunset))
   ()
   )
 
-(setq moe-timer (run-with-timer 0 (* 1 60) 'moe-theme-auto-switch))
+(setq moe-theme-switcher--timer (run-with-timer 0 (* 1 60) 'moe-theme-switcher--auto-switch))
 
 ;; [FIXME] A minor-mode to enable/disable moe-theme-switcher
 (defun moe-theme-switcher-disable ()
   (interactive)
-  (cancel-timer moe-timer))
+  (cancel-timer moe-theme-switcher--timer))
 
 (provide 'moe-theme-switcher)
